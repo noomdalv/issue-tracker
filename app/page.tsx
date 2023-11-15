8a8ad3d7a03151676f67467099acd1b6ee287cda
noomdalv <vladlunasan@gmail.com>
2023-11-14 22:25:57 -0500

    added radix ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
-  return <div>Hello World</div>;
+import { Flex, Text, Button } from "@radix-ui/themes";
+
+export default function MyApp() {
+  return (
+    <Flex direction="column" gap="2">
+      <Text>Hello from Radix Themes :)</Text>
+      <Button>Let's go</Button>
+    </Flex>
+  );
 }
