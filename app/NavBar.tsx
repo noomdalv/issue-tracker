7178eeb580d8a43fc81ee48377930eb9e1e6f611
noomdalv <vladlunasan@gmail.com>
2023-11-15 01:34:40 -0500

    customize radix ui theme

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -12,7 +12,7 @@ const NavBar = () => {
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
-  console.log(path);
+
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center text-lg">
       <Link href="/">
