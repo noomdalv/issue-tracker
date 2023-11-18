7dd8541042e7ffd04ceeba8eaf632debedf4c5f2
noomdalv <vladlunasan@gmail.com>
2023-11-17 22:51:34 -0500

    style issue detail page

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,33 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import prisma from "@/prisma/client";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
+import React from "react";
+
+interface Props {
+  params: { id: string };
+}
+
+const IssueDetails = async ({ params }: Props) => {
+  if (!parseInt(params.id)) notFound();
+
+  const issue = await prisma.issue.findUnique({
+    where: {
+      id: parseInt(params.id),
+    },
+  });
+  if (!issue) notFound();
+
+  return (
+    <div>
+      <Heading>{issue.title}</Heading>
+      <Flex gap={"3"} my={"2"}>
+        <IssueStatusBadge status={issue.status} />
+        <Text>{issue.createdAt.toDateString()}</Text>
+      </Flex>
+      <Card>{issue.description}</Card>
+    </div>
+  );
+};
+
+export default IssueDetails;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -7,7 +7,6 @@ import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000);
   return (
     <div>
       <IssueActions />
