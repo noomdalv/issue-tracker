f2d98a7a022b7f96ea7b25ff11b37a4b8ff304fc
noomdalv <vladlunasan@gmail.com>
2023-11-23 01:35:35 -0500

    Show Toast notifications

--- app/issues/[id]/AsigneeSelect.tsx
+++ app/issues/[id]/AsigneeSelect.tsx
@@ -4,6 +4,7 @@ import { Issue, User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
 import { useQuery } from "@tanstack/react-query";
 import Skeleton from "@/app/components/Skeleton";
+import toast, { Toaster } from "react-hot-toast";
 
 const AsigneeSelect = ({ issue }: { issue: Issue }) => {
   const usersQuery = () =>
@@ -24,29 +25,45 @@ const AsigneeSelect = ({ issue }: { issue: Issue }) => {
   if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <Select.Root
-      defaultValue={issue.assignedUserId || ""}
-      onValueChange={async (userId) => {
-        const assignedUserId = userId === "unassigned" ? null : userId;
-        await fetch(`/api/issues/${issue.id}`, {
-          method: "PATCH",
-          body: JSON.stringify({ assignedUserId }),
-        });
-      }}
-    >
-      <Select.Trigger placeholder="Assign a User" />
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="unassigned">Unassigned</Select.Item>
-          {data!.map((user) => (
-            <Select.Item key={user.id} value={user.id}>
-              {user.name}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Select.Root
+        defaultValue={issue.assignedUserId || ""}
+        onValueChange={async (userId) => {
+          const assignedUserId = userId === "unassigned" ? null : userId;
+          try {
+            const response = await fetch(`/api/issues/${issue.id}`, {
+              method: "PATCH",
+              body: JSON.stringify({ assignedUserId }),
+            });
+            if (!response.ok) throw new Error();
+          } catch (error) {
+            toast.error("Could not update issue");
+          }
+        }}
+      >
+        <Select.Trigger placeholder="Assign a User" />
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value="unassigned">Unassigned</Select.Item>
+            {data!.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          style: {
+            background: "#333",
+            color: "#fff",
+          },
+        }}
+      />
+    </>
   );
 };
 
