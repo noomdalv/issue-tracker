36983a8130b622ccc2e6b13dfa60d6491846aba5
noomdalv <vladlunasan@gmail.com>
2023-11-23 16:10:10 -0500

    Filtering Issues

--- app/issues/list/IssueFilterComponent.tsx
+++ app/issues/list/IssueFilterComponent.tsx
@@ -2,6 +2,7 @@
 
 import { Status } from "@prisma/client";
 import { Box, Select } from "@radix-ui/themes";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const statuses: { label: string; value?: Status }[] = [
@@ -20,14 +21,19 @@ const statuses: { label: string; value?: Status }[] = [
   },
 ];
 
-const filterByStatus = (status: Status) => {
-  console.log(status);
-};
-
 const IssueFilterComponent = () => {
+  const router = useRouter();
+
+  const filterByStatus = (status: Status | "ALL") => {
+    console.log("status", status);
+    const query = status == "ALL" ? "" : `?status=${status}`;
+    console.log("query", query);
+    router.push(`/issues/list${query}`);
+  };
+
   return (
     <Box>
-      <Select.Root defaultValue={""} onValueChange={filterByStatus}>
+      <Select.Root onValueChange={filterByStatus}>
         <Select.Trigger
           placeholder="Filter by status"
           style={{ width: "10rem" }}
