f964c4b3a01924040bf76fdfbf531e8a6be67d27
noomdalv <vladlunasan@gmail.com>
2023-11-24 18:50:41 -0500

    Build LatestIssues.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
-import { Flex, Text, Button } from "@radix-ui/themes";
 import Pagination from "./components/Pagination";
+import LatestIssues from "./LatestIssues";
 
 export default function MyApp() {
-  return <Pagination itemCount={100} pageSize={10} currentPage={5} />;
+  return (
+    <div>
+      <LatestIssues />
+      <Pagination itemCount={100} pageSize={10} currentPage={5} />
+    </div>
+  );
 }
